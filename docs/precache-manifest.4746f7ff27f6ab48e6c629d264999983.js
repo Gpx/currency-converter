b8d3101a7be723fe0b1ee876cd5d47a81dc5e667
Gpx <polvara@gmail.com>
2020-02-19 15:17:16 +0100

    fix deploy

---
+++ docs/precache-manifest.4746f7ff27f6ab48e6c629d264999983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19cb6e4153726bd54648b195f9c2301",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "4c28e52c254efe645f45",
+    "url": "/currency-converter/static/css/main.5c25f1ae.chunk.css"
+  },
+  {
+    "revision": "26fb78425231c209a8f4",
+    "url": "/currency-converter/static/js/2.f7fb5190.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/currency-converter/static/js/2.f7fb5190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c28e52c254efe645f45",
+    "url": "/currency-converter/static/js/main.bb19b538.chunk.js"
+  },
+  {
+    "revision": "5a02f36e8171cf848e21",
+    "url": "/currency-converter/static/js/runtime-main.1db9349d.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c02140b17f553a4bfc2158a6c92ed6b.js"
+  "/currency-converter/precache-manifest.4746f7ff27f6ab48e6c629d264999983.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/currency-converter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
